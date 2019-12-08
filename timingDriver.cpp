0c6b7c8ef52bc089b73a734b5afce9a25d2bf133
Zac Clement <zacclement0@gmail.com>
2019-12-08 11:23:37 -0700

    Expanded driver

--- timingDriver.cpp
+++ timingDriver.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <ctime>
 #include <chrono>
 #include <fstream>
 #include <math.h>
@@ -7,32 +6,37 @@
 #include "hashProbe.hpp"
 #include "hash_BST.hpp"
 #include "hashFunction.hpp"
+#include "hashCuckoo.hpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    clock_t clockStart;
-    int startTime, endTime;
-    double execTime;
     ifstream dataFile;
     string fileName = argv[1];
     ifstream inputFile (fileName);
+
+    float loadFactor = 0.9;
 
     cout << "Creating a table with LL collision implementation and hash function 0" << endl;
     HashTableLL* llHash0 = new HashTableLL(0);
     cout << "Creating a table with LL collision implemenation and hash function 1" << endl;
     HashTableLL* llHash1 = new HashTableLL(1);
+
     cout << "Creating a table with BST collision and hash function 0" << endl;
     HashTableBST* bstHash0 = new HashTableBST(0);
     cout << "Creating a table with BST collision and hash function 1" << endl;
     HashTableBST* bstHash1 = new HashTableBST(1);
+
     cout << "Creating a table with linear probing and hash function 0" << endl;
     HashTableProbe* probeHash0 = new HashTableProbe(0);
     cout << "Creating a table with linear probing and hash function 1" << endl;
     HashTableProbe* probeHash1 = new HashTableProbe(1);    
 
-    cout << "Getting a load factor of 0.1" << endl;
-    int maxData1 = floor(0.1 * TABLE_SIZE);
+    cout << "Creating a table with cuckoo hasing" << endl;
+    HashTableCuckoo* cuckooHash = new HashTableCuckoo();
+
+    cout << "Getting a load factor of " << loadFactor << endl;
+    int maxData1 = floor(loadFactor * TABLE_SIZE);
     int count  = 0;
     string line;
     int* firstDataSet = new int[100];
@@ -53,42 +57,204 @@ int main(int argc, char const *argv[]) {
             firstDataSet[count-maxData1] = data;
         }
         // cout << count << endl;
+        // Don't increment if a duplicate is tried to be added
         if (insertFail);
         else count++;
     }
 
+    /** =========================================== INSERTIONS =========================================== **/
     // Insert 100 into llHash0
-    double* ll0InsertTimes = new double[100];
+    auto startTime = chrono::high_resolution_clock::now();
     for (int i = 0; i < 100; i++ ) {
-        auto startTime = chrono::high_resolution_clock::now();
         llHash0->insertItem(firstDataSet[i]);
-        auto endTime = std::chrono::high_resolution_clock::now();
-        chrono::duration<double> execTime = endTime-startTime;
-        ll0InsertTimes[i] = execTime.count();
     }
-    double ll0Sum = 0;
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nllHash0 insert average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Insert 100 into llHash1
+    startTime = chrono::high_resolution_clock::now();
     for (int i = 0; i < 100; i++ ) {
-        cout << " + " << ll0InsertTimes[i];
-        ll0Sum += ll0InsertTimes[i];
+        llHash1->insertItem(firstDataSet[i]);
     }
-    cout << "\nllHash0 average execution time: " << (ll0Sum / 100) << endl;
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nllHash1 insert average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Insert 100 into bstHash0
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        bstHash0->insertItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nbstHash0 insert average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Insert 100 into bstHash1
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        bstHash1->insertItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nbstHash1 insert average execution time: " << execTime << " nanoseconds" << endl;
 
     // Insert 100 into probe0
-    double* probe0InsertTimes = new double[100];
+    startTime = chrono::high_resolution_clock::now();
     for (int i = 0; i < 100; i++ ) {
-        auto startTime = chrono::system_clock::now();
         probeHash0->insertItem(firstDataSet[i]);
-        auto endTime = std::chrono::system_clock::now();
-        chrono::duration<double> execTime = endTime-startTime;
-        probe0InsertTimes[i] = execTime.count();
     }
-    double probe0Sum = 0;
-    for (int i = 0; i < 100; i++ ) {
-        cout << " + " << probe0InsertTimes[i];
-        probe0Sum += probe0InsertTimes[i];
-    }
-    cout << "\nprobeHash0 average execution time: " << (probe0Sum / 100) << endl;
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nprobeHash0 insert average execution time: " << (execTime) << " nanoseconds" << endl;
 
+    // Insert 100 into probe1
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        probeHash1->insertItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nprobeHash1 insert average execution time: " << (execTime) << " nanoseconds" << endl;
+
+    // Insert 100 into cuckooo
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        cuckooHash->insertItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\ncuckooHash insert average execution time: " << (execTime) << " nanoseconds" << endl;
+
+    /** =========================================== LOOKUPS =========================================== **/
+    // Search 100 in llHash0
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        llHash0->searchItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nllHash0 lookup average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Search 100 in llHash1
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        llHash1->searchItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nllHash1 lookup average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Search 100 in bstHash0
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        bstHash0->searchItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nbstHash0 lookup average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Search 100 in bstHash1
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        bstHash1->searchItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nbstHash1 lookup average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Search 100 in probe0
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        probeHash0->searchItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nprobeHash0 lookup average execution time: " << (execTime) << " nanoseconds" << endl;
+
+    // Search 100 in probe1
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        probeHash1->searchItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nprobeHash1 lookup average execution time: " << (execTime) << " nanoseconds" << endl;
+
+    // Search 100 in cuckooo
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        cuckooHash->searchItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\ncuckooHash lookup average execution time: " << (execTime) << " nanoseconds" << endl;
+
+    /** =========================================== DELETIONS =========================================== **/
+    // Delete 100 from llHash0
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        llHash0->deleteItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nllHash0 delete average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Delete 100 from llHash1
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        llHash1->deleteItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nllHash1 delete average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Delete 100 from bstHash0
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        bstHash0->deleteItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nbstHash0 delete average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Delete 100 from bstHash1
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        bstHash1->deleteItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nbstHash1 delete average execution time: " << execTime << " nanoseconds" << endl;
+
+    // Delete 100 from probe0
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        probeHash0->deleteItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nprobeHash0 delete average execution time: " << (execTime) << " nanoseconds" << endl;
+
+    // Delete 100 from probe1
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        probeHash1->deleteItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\nprobeHash1 delete average execution time: " << (execTime) << " nanoseconds" << endl;
+
+    // Delete 100 from cuckooo
+    startTime = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 100; i++ ) {
+        cuckooHash->deleteItem(firstDataSet[i]);
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    execTime = chrono::duration_cast<std::chrono::nanoseconds> (endTime-startTime).count();
+    cout << "\ncuckooHash delete average execution time: " << (execTime) << " nanoseconds" << endl;
+
+    // Printing hash tables for debugging
     // cout << "\nPrinting LL Hash Table with Hash Function 0: " << endl;
     // llHash0->printTable();
     // cout << "\nPrinting LL Hash Table with Hash Function 1: " << endl;
@@ -101,5 +267,7 @@ int main(int argc, char const *argv[]) {
     // probeHash0->printTable();
     // cout << "\nPrinting Probing Hash Table with Hash Function 1: " << endl;
     // probeHash1->printTable();
+    // cout << "\nPrinting Cuckoo Hash Table: " << endl;
+    // cuckooHash->printTables();
     return 0;
 }
